6f3ca134b52c283fcc39f14539652770333005ff
Stephen Done <srdone@gmail.com>
2015-02-06 21:28:35 -0700

    working - fixed misuse of append

--- app.js
+++ app.js
@@ -6,13 +6,32 @@ $(document).ready(function myDataGenerator () {
 
 		var token = $('#token').val();
 
-		$.ajax({
-			method: 'GET',
-			url: 'https://graph.facebook.com/v2.2/me/friends?access_token=' + token + '&format=json&method=get&pretty=0&suppress_http_code=1'
-		})
-		.done(function (data) {
-			$('#dataviz').text(data.summary.total_count);
-		});
+		var getLikes = function () {
+			$.ajax({
+				method: 'GET',
+				url: 'https://graph.facebook.com/v2.2/me/likes?access_token=' + token
+			})
+			.done(function (data) {
+				console.log(data.data);
+				$('#dataviz').append('<p>The first page of likes that Facebook returned was:</p>');
+				data.data.forEach(function(c) {
+					$('#dataviz').append('<p>' + c.name + '</p>');
+				});
+			});
+		};
+
+		var getFriends = function () {
+			$.ajax({
+				method: 'GET',
+				url: 'https://graph.facebook.com/v2.2/me/friends?access_token=' + token + '&format=json&method=get&pretty=0&suppress_http_code=1'
+			})
+			.done(function (data) {
+				$('#dataviz').append('<p>You have ' + data.summary.total_count + ' friends on facebook</p>');
+				setTimeout(getLikes(), 1);
+			});
+		};
+
+		getFriends();
 
 	};
 
